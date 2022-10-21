c3fb6c44d1b50bd2d85cd7b3a8079389378d756e
高津碧 <89884810+aoi-1100901@users.noreply.github.com>
2022-10-21 20:40:00 +0900

    Delete script.js

--- キャンフェス_サイト/script.js
+++
@@ -1,7 +0,0 @@
-const paragraph = document.getElementById('paragraph');
-const imgRival = document.getElementById('imgRival');
-const imgGu = document.getElementById('imgGu');
-const imgChoki = document.getElementById('imgChoki');
-const imgPa = document.getElementById('imgPa');
-const reStartButton = document.getElementById('reStart');
-const scoreButton = document.getElementById('score');
