fdecd6e2bde2a616c37bae3727489440c2947468
高津碧 <89884810+aoi-1100901@users.noreply.github.com>
2022-10-21 16:45:45 +0900

    Add files via upload

---
+++ キャンフェス_サイト/script.js
@@ -0,0 +1,7 @@
+const paragraph = document.getElementById('paragraph');
+const imgRival = document.getElementById('imgRival');
+const imgGu = document.getElementById('imgGu');
+const imgChoki = document.getElementById('imgChoki');
+const imgPa = document.getElementById('imgPa');
+const reStartButton = document.getElementById('reStart');
+const scoreButton = document.getElementById('score');
